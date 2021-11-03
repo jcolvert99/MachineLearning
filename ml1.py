db51c474862186f496470c82c114e663da16cb14
John Colvert <john_colvert2@baylor.edu>
2021-11-03 17:14:26 -0500

    commit

--- ml1.py
+++ ml1.py
@@ -73,3 +73,5 @@ confusion_df = pd.DataFrame(confusion, index=range(10), columns=range(10))
 
 figure = plt2.figure(figsize=(7,6))
 axes = sns.heatmap(confusion_df, annot=True, cmap=plt2.cm.nipy_spectral_r)
+
+plt2.show()
